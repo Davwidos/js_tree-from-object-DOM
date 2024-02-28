6080d939cc4113fbe6aa84b26d63853fb714677a
Davwidos <d.jurkowski015@gmail.com>
2024-02-28 10:43:39 +0100

    make build

---
+++ index.7d319ab5.js
@@ -0,0 +1,2 @@
+!function e(n,t){if("object"!=typeof t||0===Object.keys(t).length)return;let r=document.createElement("UL");for(let n in t){let a=document.createElement("LI");a.innerHTML=n,r.append(a),e(a,t[n])}n.append(r)}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.7d319ab5.js.map
